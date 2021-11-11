1a829ca76c7cbd8649333b5d5bb316608e0b14d7
Den-programmer <denis_grigorov@gmail.com>
2021-11-11 13:03:02 +0200

    Updates

---
+++ precache-manifest.1b86b41cd01ec946749b2fc06ff09740.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228e2a3adfca8108e3c5e326388e3b7e",
+    "url": "/Internet-store/index.html"
+  },
+  {
+    "revision": "6f41935e75cbe59a5594",
+    "url": "/Internet-store/static/css/main.b439d1ec.chunk.css"
+  },
+  {
+    "revision": "e78266a1b27712d1ae4f",
+    "url": "/Internet-store/static/js/2.7f7c2be8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Internet-store/static/js/2.7f7c2be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f41935e75cbe59a5594",
+    "url": "/Internet-store/static/js/main.39be0f4d.chunk.js"
+  },
+  {
+    "revision": "06dd2ecb40aa4274cd2e",
+    "url": "/Internet-store/static/js/runtime-main.e299cd68.js"
+  },
+  {
+    "revision": "3002308376d89f440737f5e130debe60",
+    "url": "/Internet-store/static/media/BLS_Tulip_BrownCream.30023083.png"
+  },
+  {
+    "revision": "51cbf03ab5c7d5bf1ed90b35bb0b96e3",
+    "url": "/Internet-store/static/media/Cybershot_Digital_Camera.51cbf03a.png"
+  },
+  {
+    "revision": "6063125ded7379f4cc4fa6f4197346e1",
+    "url": "/Internet-store/static/media/Flipkart_Buildskill_13MM_Powerful_Reversible_Impact_Heavy_Duty.6063125d.png"
+  },
+  {
+    "revision": "0caed230fc0538eb3e96b92b1c75931d",
+    "url": "/Internet-store/static/media/Huawei_honour_U19.0caed230.png"
+  },
+  {
+    "revision": "fe13ef3002cdf63ea63cd2a8bd6a3490",
+    "url": "/Internet-store/static/media/IPnone7_32GB_Black.fe13ef30.png"
+  },
+  {
+    "revision": "781de5ca96e1b8d7f1bd32266d91a1d5",
+    "url": "/Internet-store/static/media/Jios_Dual_Speaker_Black.781de5ca.png"
+  },
+  {
+    "revision": "089579d620cc6c9cb4485f7e0a4f2043",
+    "url": "/Internet-store/static/media/Plastic_Steel_Flask.089579d6.png"
+  },
+  {
+    "revision": "1ec7f0bb31a9858edafdcd67f5db2f4b",
+    "url": "/Internet-store/static/media/Sasik_External_Disk_500GB.1ec7f0bb.jpg"
+  },
+  {
+    "revision": "cd84fdc3220e292dc1c071c8de8f0827",
+    "url": "/Internet-store/static/media/Shaving_Cream_100gm.cd84fdc3.png"
+  },
+  {
+    "revision": "f59b9cb530084f9aca82d8ec5f4d82cb",
+    "url": "/Internet-store/static/media/Skinn_Celeste_Woman.f59b9cb5.png"
+  },
+  {
+    "revision": "02a9d5256dbd3e850f6bb294e8328612",
+    "url": "/Internet-store/static/media/Slooven_Stripped_T_shirt.02a9d525.jpg"
+  },
+  {
+    "revision": "c21de092ebfabe9550a348f644c59b52",
+    "url": "/Internet-store/static/media/XiaomiMI9SEDual_64GB_6GB_Ram.c21de092.png"
+  },
+  {
+    "revision": "a8aec3660b1d6941c49a289a0fabf5ef",
+    "url": "/Internet-store/static/media/Yunique_Plus_Hair_Styler.a8aec366.jpg"
+  },
+  {
+    "revision": "e335fdec361899aec0b1d64bce29a509",
+    "url": "/Internet-store/static/media/Yunique_Plus_Hair_Styler.e335fdec.png"
+  },
+  {
+    "revision": "6c58bb9569a13e8f5bdf7d86abf74a88",
+    "url": "/Internet-store/static/media/appleIphone5s_16GB.6c58bb95.png"
+  },
+  {
+    "revision": "b8f79f5ff30f05f01d5558071545aa76",
+    "url": "/Internet-store/static/media/appleWatch.b8f79f5f.png"
+  },
+  {
+    "revision": "64dbc4eab05b0cd9019ca748e424cca6",
+    "url": "/Internet-store/static/media/appleWatch2.64dbc4ea.jpg"
+  },
+  {
+    "revision": "9d20ee2dea191f066df867e867f618eb",
+    "url": "/Internet-store/static/media/appleWatch3.9d20ee2d.jpg"
+  },
+  {
+    "revision": "571d08af78d8444b8701f53c7d4217d1",
+    "url": "/Internet-store/static/media/appleWatch4.571d08af.jpg"
+  },
+  {
+    "revision": "45e1883f0287e6ffeecd497c4af44ecd",
+    "url": "/Internet-store/static/media/avatar.45e1883f.jpg"
+  },
+  {
+    "revision": "707b6ef8874ecc381d331a9a95849301",
+    "url": "/Internet-store/static/media/avatar10.707b6ef8.png"
+  },
+  {
+    "revision": "696bd41b856051f432db48bec82a1fc6",
+    "url": "/Internet-store/static/media/avatar9.696bd41b.jpg"
+  },
+  {
+    "revision": "02a1a91c892299c41c15615a3328f0d2",
+    "url": "/Internet-store/static/media/basket.02a1a91c.png"
+  },
+  {
+    "revision": "41479f3e24735063dde7cd2db85824c1",
+    "url": "/Internet-store/static/media/basket.41479f3e.png"
+  },
+  {
+    "revision": "5ba247a0fa08ae93f8bd123942c147de",
+    "url": "/Internet-store/static/media/basket.5ba247a0.png"
+  },
+  {
+    "revision": "2d6468392816aa4556b1f6fd530c10cb",
+    "url": "/Internet-store/static/media/blackTShirt.2d646839.jpg"
+  },
+  {
+    "revision": "d9493e8a089c9ddc6e2537a2cd79c4f9",
+    "url": "/Internet-store/static/media/cards.d9493e8a.png"
+  },
+  {
+    "revision": "1d0ebda68841272dfd8380f43ffc1f4a",
+    "url": "/Internet-store/static/media/cyberShotCable.1d0ebda6.png"
+  },
+  {
+    "revision": "61e7728fc1152a4cf3578bcea474911e",
+    "url": "/Internet-store/static/media/envelope.61e7728f.png"
+  },
+  {
+    "revision": "477d2a9c65ed6aa3478d13a2631f2637",
+    "url": "/Internet-store/static/media/facebook.477d2a9c.png"
+  },
+  {
+    "revision": "c6c224fe88bb90269784fb6dc1d9c1c7",
+    "url": "/Internet-store/static/media/facebook.c6c224fe.png"
+  },
+  {
+    "revision": "902fb616a1ec443480bf7bbf05fad430",
+    "url": "/Internet-store/static/media/facebookSquare.902fb616.png"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/Internet-store/static/media/getFetch.51c934af.cjs"
+  },
+  {
+    "revision": "37ed1b47f5db69783ccc777c246302cc",
+    "url": "/Internet-store/static/media/girl.37ed1b47.jpg"
+  },
+  {
+    "revision": "53a95fc17d674f1f69f7eef4437d3117",
+    "url": "/Internet-store/static/media/googlePlusSquare.53a95fc1.png"
+  },
+  {
+    "revision": "6b5a24539e5af775c627e92513276dd2",
+    "url": "/Internet-store/static/media/googlePlusSquare2.6b5a2453.png"
+  },
+  {
+    "revision": "82df2bb255f57b48a54e71264b9d90bf",
+    "url": "/Internet-store/static/media/greyStar.82df2bb2.png"
+  },
+  {
+    "revision": "d08a685d791de38b347ffab9df6b5bbd",
+    "url": "/Internet-store/static/media/handwear.d08a685d.png"
+  },
+  {
+    "revision": "3473cc3b8e791b77ab5ba21b7da42774",
+    "url": "/Internet-store/static/media/heart.3473cc3b.png"
+  },
+  {
+    "revision": "b542becc663aea6f6977b8451634acd2",
+    "url": "/Internet-store/static/media/heartColored.b542becc.png"
+  },
+  {
+    "revision": "418d27f41db282d081fdaa37e6fcc3a0",
+    "url": "/Internet-store/static/media/heartUncolored.418d27f4.png"
+  },
+  {
+    "revision": "96cde4ed9ac37f2c555fbdd50e92db30",
+    "url": "/Internet-store/static/media/instagram.96cde4ed.png"
+  },
+  {
+    "revision": "e922f4fed2ce4a8a3e1376b997c00a02",
+    "url": "/Internet-store/static/media/linkedInSquare.e922f4fe.png"
+  },
+  {
+    "revision": "b5043d1031b66cf267ae6f7ea8ca5257",
+    "url": "/Internet-store/static/media/macbookPro.b5043d10.png"
+  },
+  {
+    "revision": "6419356d7234941c112bae606371113e",
+    "url": "/Internet-store/static/media/phone-samsung-galaxy-note-20-ultra-8gb-256gb.6419356d.png"
+  },
+  {
+    "revision": "ada615b23cc4a85da8a0da8ebb426c75",
+    "url": "/Internet-store/static/media/photo.ada615b2.jpg"
+  },
+  {
+    "revision": "ada615b23cc4a85da8a0da8ebb426c75",
+    "url": "/Internet-store/static/media/photoTemplate.ada615b2.jpg"
+  },
+  {
+    "revision": "07009b300d7dea9aadb0cb5b05f20e0e",
+    "url": "/Internet-store/static/media/pinterest.07009b30.png"
+  },
+  {
+    "revision": "f8bae2b5e0c4d684ee50ddfa23e6b05f",
+    "url": "/Internet-store/static/media/pinterest.f8bae2b5.png"
+  },
+  {
+    "revision": "e1bc2161e7896360da45fd53f2262df5",
+    "url": "/Internet-store/static/media/puma.e1bc2161.png"
+  },
+  {
+    "revision": "8ce1fedf8b63de51fcc755959bd1dfae",
+    "url": "/Internet-store/static/media/remove.8ce1fedf.png"
+  },
+  {
+    "revision": "83bdfa84d46a2ad7e4074bbd9ab1b3d2",
+    "url": "/Internet-store/static/media/remove2.83bdfa84.png"
+  },
+  {
+    "revision": "c19415e7a7434be92ec9bca26143c1cd",
+    "url": "/Internet-store/static/media/sadSmile.c19415e7.jpg"
+  },
+  {
+    "revision": "cb5a6c864510cba5bff7d1e4b852f962",
+    "url": "/Internet-store/static/media/service1.cb5a6c86.png"
+  },
+  {
+    "revision": "6f847859af0897bae2a68717dd512657",
+    "url": "/Internet-store/static/media/service2.6f847859.png"
+  },
+  {
+    "revision": "012007ceb86f7dd26e81ef5c59f2e9b3",
+    "url": "/Internet-store/static/media/service3.012007ce.png"
+  },
+  {
+    "revision": "0d2fb38cb91f5cb6fb46558c08a020e9",
+    "url": "/Internet-store/static/media/shield.0d2fb38c.png"
+  },
+  {
+    "revision": "254abe7c119cd02725814e29ad5c19bf",
+    "url": "/Internet-store/static/media/sliderItem1.254abe7c.jpg"
+  },
+  {
+    "revision": "a889b32f5e4fbe0f7f8ac5c7b0cfcf78",
+    "url": "/Internet-store/static/media/sliderItem2.a889b32f.jpg"
+  },
+  {
+    "revision": "92ccc56d188a995e7ecc7591bcba7f94",
+    "url": "/Internet-store/static/media/sliderItem3.92ccc56d.jpg"
+  },
+  {
+    "revision": "2ab138942aff2671889effd2e9715bdb",
+    "url": "/Internet-store/static/media/star.2ab13894.png"
+  },
+  {
+    "revision": "f06a21bab3a729bbf5914aea0ea32a93",
+    "url": "/Internet-store/static/media/supermarketPhoto.f06a21ba.jpg"
+  },
+  {
+    "revision": "3fae98d8f1ce6d4121ee3955a8a331f5",
+    "url": "/Internet-store/static/media/twitter.3fae98d8.png"
+  },
+  {
+    "revision": "415ca242ab123e792905bf265c215868",
+    "url": "/Internet-store/static/media/twitter.415ca242.png"
+  },
+  {
+    "revision": "c6232a00cf4b3d282a3cd239ff2fb2be",
+    "url": "/Internet-store/static/media/twitterSquare.c6232a00.png"
+  },
+  {
+    "revision": "13a7910110e9cb52acde8343f03c13d9",
+    "url": "/Internet-store/static/media/twitterSquare2.13a79101.png"
+  },
+  {
+    "revision": "6437646123e934525c618c85c598d045",
+    "url": "/Internet-store/static/media/victory.64376461.png"
+  }
+]);
